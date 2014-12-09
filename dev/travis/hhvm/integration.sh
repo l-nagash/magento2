c7db24703fafe2976ce7a15a732611421b2e4da2
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2014-12-09 22:12:39 +0200

    Remove export from config

--- dev/travis/hhvm/integration.sh
+++ dev/travis/hhvm/integration.sh
@@ -4,4 +4,4 @@ echo 'alias phpunit="hhvm -c $TRAVIS_BUILD_DIR/dev/travis/php.ini -v Eval.Jit=1 
 . ~/.bashrc
 cd $TRAVIS_BUILD_DIR/dev/tests/integration/
 #hhvm -c $TRAVIS_BUILD_DIR/dev/travis/php.ini -v Eval.Jit=false $TRAVIS_BUILD_DIR/vendor/phpunit/phpunit/phpunit -c ./phpunit.xml.dist
-phpunit -c ./phpunit.xml.dist --testsuite $TEST_SUITE_INT
+hhvm -c $TRAVIS_BUILD_DIR/dev/travis/php.ini -v Eval.Jit=1 $TRAVIS_BUILD_DIR/vendor/phpunit --testsuite $TEST_SUITE_INT
