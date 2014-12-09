2b5ba471794e48566b1d6a14d292d798829f0046
Sanuch <sanuch@gmail.com>
2014-12-09 07:08:58 -0800

    Change HHVM parameters

--- dev/travis/hhvm/unit.sh
+++ dev/travis/hhvm/unit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'alias phpunit="hhvm -v Eval.Jit=false $TRAVIS_BUILD_DIR/vendor/phpunit/phpunit/phpunit"' >> ~/.bashrc
+echo 'alias phpunit="hhvm -v Eval.Jit=1 $TRAVIS_BUILD_DIR/vendor/phpunit/phpunit/phpunit"' >> ~/.bashrc
 . ~/.bashrc
 cd $TRAVIS_BUILD_DIR/dev/tests/unit/
 for i in $(find ./testsuite -maxdepth 2 -mindepth 2 -type d | grep -v '_' | sort); do
