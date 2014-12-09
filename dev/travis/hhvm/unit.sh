62d9985596165309a9c235dfa6c9f9ffc388aca7
Sanuch <sanuch@gmail.com>
2014-12-09 07:00:32 -0800

    Support HHVM

---
+++ dev/travis/hhvm/unit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo 'alias phpunit="hhvm -v Eval.Jit=false $TRAVIS_BUILD_DIR/vendor/phpunit/phpunit/phpunit"' >> ~/.bashrc
+. ~/.bashrc
+cd $TRAVIS_BUILD_DIR/dev/tests/unit/
+for i in $(find ./testsuite -maxdepth 2 -mindepth 2 -type d | grep -v '_' | sort); do
+    phpunit -c ./phpunit.xml.dist $i
+    echo $i
+    echo ""
+done
