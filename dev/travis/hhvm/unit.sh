3b5a0034d8d77196db6e7b6ff98f8322c31086bc
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2014-12-09 20:48:24 +0200

    Remove export from config

--- dev/travis/hhvm/unit.sh
+++ dev/travis/hhvm/unit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo 'alias phpunit="hhvm -c $TRAVIS_BUILD_DIR/dev/travis/php.ini -v Eval.Jit=1 $TRAVIS_BUILD_DIR/vendor/phpunit/phpunit/phpunit"' >> ~/.bashrc
-. ~/.bashrc
+#echo 'alias phpunit="hhvm -c $TRAVIS_BUILD_DIR/dev/travis/php.ini -v Eval.Jit=1 $TRAVIS_BUILD_DIR/vendor/phpunit/phpunit/phpunit"' >> ~/.bashrc
+#. ~/.bashrc
 cd $TRAVIS_BUILD_DIR/dev/tests/unit/
 for i in $(find ./testsuite -maxdepth 2 -mindepth 2 -type d | grep -v '_' | sort); do
-    phpunit -c ./phpunit.xml.dist $i
+    hhvm -c $TRAVIS_BUILD_DIR/dev/travis/php.ini -v Eval.Jit=1 $TRAVIS_BUILD_DIR/vendor/phpunit/phpunit/phpunit $i
     echo $i
     echo ""
 done
