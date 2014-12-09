903a09d3eba5fa7e9f7ee50c837c3d9e1abb58a0
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2014-12-09 19:50:56 +0200

    Fix using configuration for hhvm

--- dev/travis/hhvm/integration.sh
+++ dev/travis/hhvm/integration.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo 'alias phpunit="hhvm -v Eval.Jit=1 $TRAVIS_BUILD_DIR/vendor/phpunit/phpunit/phpunit"' >> ~/.bashrc
+echo 'alias phpunit="hhvm -c $TRAVIS_BUILD_DIR/dev/travis/php.ini -v Eval.Jit=1 $TRAVIS_BUILD_DIR/vendor/phpunit/phpunit/phpunit"' >> ~/.bashrc
 . ~/.bashrc
 cd $TRAVIS_BUILD_DIR/dev/tests/integration/
 #hhvm -c $TRAVIS_BUILD_DIR/dev/travis/php.ini -v Eval.Jit=false $TRAVIS_BUILD_DIR/vendor/phpunit/phpunit/phpunit -c ./phpunit.xml.dist
-phpunit -c ./phpunit.xml.dist --testsuite $TEST_SUITE
+phpunit -c ./phpunit.xml.dist --testsuite $TEST_SUITE_INT
