d983afbac66dfe25a584254ad4340d227274f251
Sanuch <Sanuch@users.noreply.github.com>
2014-12-11 09:51:16 +0200

    Add sudo

--- dev/travis/prepare.sh
+++ dev/travis/prepare.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-apt-get remove --purge mysql-common mysql-server-5.5 mysql-server-core-5.5 mysql-client-5.5 mysql-client-core-5.5
-apt-get autoremove
-apt-get autoclean
-apt-add-repository ppa:ondrej/mysql-5.6 -y
-apt-get update
-apt-get install mysql-server-5.6 mysql-client-5.6 hhvm-dbg
+sudo apt-get remove --purge mysql-common mysql-server-5.5 mysql-server-core-5.5 mysql-client-5.5 mysql-client-core-5.5
+sudo apt-get autoremove
+sudo apt-get autoclean
+sudo apt-add-repository ppa:ondrej/mysql-5.6 -y
+sudo apt-get update
+sudo apt-get install mysql-server-5.6 mysql-client-5.6 hhvm-dbg
