549756e7def55af649ea90c4190143db4dcf8d5f
Sanuch <Sanuch@users.noreply.github.com>
2014-12-11 09:27:52 +0200

    Fix path variable

--- dev/travis/hhvm.sh
+++ dev/travis/hhvm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-pwd
-cat $TRAVIS_BUILD_DIR/dev/travis/php.ini >> /etc/hhvm/php.ini;
+currentPath=$(pwd)
+echo $currentPath;
+echo $TRAVIS_BUILD_DIR;
+cat ${TRAVIS_BUILD_DIR}/dev/travis/php.ini >> /etc/hhvm/php.ini;
 #    cat $TRAVIS_BUILD_DIR/dev/travis/php.ini >> ~/.phpenv/versions/$(phpenv version-name)/php.ini;
