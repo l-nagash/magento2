a0a55c665eb64bec63c77725e43c69e238735ebc
Oleksandr Ivashchenko <oivashchenko@ebay.com>
2015-01-15 09:44:53 +0200

    Support HHVM

--- dev/travis/install-mysql-5.6.sh
+++ dev/travis/install-mysql-5.6.sh
@@ -3,9 +3,8 @@
 set -e
 
 sudo apt-get remove -qq --purge mysql-common mysql-server-5.5 mysql-server-core-5.5 mysql-client-5.5 mysql-client-core-5.5
-#sudo apt-get autoremove -qq
+sudo apt-get autoremove -qq
 sudo apt-get autoclean -qq
 sudo apt-add-repository ppa:ondrej/mysql-5.6 -y
 sudo apt-get update -qq
-sudo apt-get install -y -qq mysql-server-5.6 mysql-client-5.6
-#hhvm-dbg
+sudo apt-get install -y -qq mysql-server-5.6 mysql-client-5.6 hhvm-dbg
